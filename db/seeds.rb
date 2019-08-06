9b894ce03ada23830bcc854b45d988123ac484a5
TsuyoshiHara <chloe463@gmail.com>
2019-08-07 01:36:10 +0900

    build: Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..10).each do |questionnaire_id|
+  questionnaire = Questionnaire.create({
+    title: "Questionnaire.#{questionnaire_id}",
+    description: "This is a suvey for ...",
+    state_cd: 1,
+    start_at: Date.new(2019, 8, 1),
+    end_at: Date.new(2020, 8, 1),
+  })
+
+  (1..10).each do |question_id|
+    question = Question.create({
+      questionnaire_id: questionnaire.id,
+      text: "Question.#{question_id}",
+      type_cd: question_id % 4,
+      required: true,
+    })
+
+    (1..4).each do |option_id|
+      option = Option.create({
+        question_id: question.id,
+        text: "Option.#{option_id}",
+      })
+    end
+  end
+end
